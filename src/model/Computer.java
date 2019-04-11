a19db3e87ac4486c3872a423b4c894ac4628f04f
Manuel Balanta Quintero <1005943823@academico.icesi.edu.co>
2019-04-11 18:27:06 -0500

    Set and get method implemented

--- src/model/Computer.java
+++ src/model/Computer.java
@@ -1,5 +1,45 @@
 package model;
 
 public class Computer {
+	
+	private int hd;
+	private int ram;
+	private String ip;
+	private Computer next;
+	private Computer previous;
+	
+	public Computer(int h, int r, String ip) {
+		hd = h;
+		ram = h;
+		this.ip = ip;
+	}
+	
+	public int getHd() {
+		return hd;
+	}
+	
+	public int getRam() {
+		return ram;
+	}
+	
+	public String getIp() {
+		return ip;
+	}
+	
+	public Computer gerNext() {
+		return next;
+	}
+	
+	public Computer getPrevious() {
+		return previous;
+	}
+	
+	public void setNext(Computer c) {
+		next = c;
+	}
+	
+	public void setPrevious(Computer c) {
+		previous = c;
+	}
 
 }
